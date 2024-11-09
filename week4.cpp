280d42d02360d54b615e534675a2cd25f2cb874e
Jordon Marchesano <102629027+cHoMpDiSs@users.noreply.github.com>
2024-11-08 22:36:58 -0800

    Update week4.cpp

--- week4.cpp
+++ week4.cpp
@@ -1,94 +1,74 @@
-//
-//  main.cpp
-//  week4
-//
-//  Created by Jordon Marchesano on 11/7/24.
-//
-
 #include <iostream>
-#include <iomanip>
 #include <cctype>
+#include <iomanip>
 #include <string>
 using namespace std;
 
-int readDials(string num);
-int toDigit(char ch);
-
-int toDigit(char ch) {
+char dig1, dig2, dig3, hyph, dig4, dig5, dig6, dig7;
+int toDigit(char &ch) {
     char c = toupper(ch);
     if (!isalnum(static_cast<unsigned char>(c))) {
         return -1;
     }
     switch (c) {
         case 'A': case 'B': case 'C':
-            return 2;
+           ch = '2';
+            break;
         case 'D': case 'E': case 'F':
-            return 3;
+            ch = '3';
+            break;
         case 'G': case 'H': case 'I':
-            return 4;
+            ch = '4';
+            break;
         case 'J': case 'K': case 'L':
-            return 5;
+            ch = '5';
+            break;
         case 'M': case 'N': case 'O':
-            return 6;
+            ch = '6';
+            break;
         case 'P': case 'Q': case 'R': case 'S':
-            return 7;
+            ch = '7';
+            break;
         case 'T': case 'U': case 'V':
-            return 8;
+            ch = '8';
+            break;
         case 'W': case 'X': case 'Y': case 'Z':
-            return 9;
+            ch = '9';
+            break;
         default:
             return c - '0';
 
     }
+    return 0;
 }
-
-int readDials(string num){
-    long int numLength = num.length();
-    vector<int> convertedNum;
-     if(numLength < 8){
-         return 0;
-     }
-     else if (num[0] == '0'){
-         return -2;
-     }
-  
-     else if (num[3] != '-'){
-         return -4;
-     }
-     else{
-         for (int i = 0; i <= 3; i++){
-             cout << num[i] << " Num I" << endl;
-             if (num[i] == '5'){
-                 continue;
-             }
-             else if(i > 3 && num[i] != '5'){
-                 break;
-             }
-             return -3;
-         }
-     }
-    for (int i = 0; i < numLength; i++){
-        if (i == 3){
-            continue;
-        }
-        else{
-            convertedNum.push_back(toDigit(num[i]));
-        }
+int readDials(){
+//    char thing;
+    //Initialize variables and get user input for phone #
+    int verif1, verif2, verif3, verif4, verif5, verif6, verif7;
+    verif1 = toDigit(dig1);
+    verif2 = toDigit(dig2);
+    verif3 = toDigit(dig3);
+    verif4 = toDigit(dig4);
+    verif5 = toDigit(dig5);
+    verif6 = toDigit(dig6);
+    verif7 = toDigit(dig7);
+    if (dig1 == -1 || dig2 == -1 || dig3 == -1 || verif4 == -1|| verif5 ==-1 || verif6 == -1 || verif7 == -1){
+        return -1;
     }
-    string dialedNumber;
-    for (int i = 0; i < convertedNum.size(); ++i) {
-        if (i == 3) {
-                dialedNumber += '-';
-            }
-        dialedNumber += to_string(convertedNum[i]);
-        }
-        cout << "Dialed number: " << dialedNumber << endl;
+    if (dig1 == '0'){
+        return -2;
+    }
+    if (dig1 == '5' && dig2 == '5' && dig3 == '5'){
+        return -3;
+    }
+    if (hyph != '-'){
+        return -4;
+    }
     return 0;
 }
 
-string phoneNumber;
 
-int main(int argc, const char * argv[]) {
+int main(){
     int studentId1, studentId2;
     studentId1 = 900536880;
     studentId2 = 900914917;
@@ -99,22 +79,20 @@ int main(int argc, const char * argv[]) {
     cout  << left << setw(18) << "Student ID: " << studentId1 << endl;
     cout  << left << setw(18) << "Student Name: " << nameStudent2 << endl;
     cout  << left << setw(18) << "Student ID: " << studentId2 << "\n" << endl;
-   
- 
+    int verify;
     while (true){
-        cout << "Enter a phone number (Q to quit): ";
-        cin >> phoneNumber;
-        if (phoneNumber == "q" || phoneNumber == "Q"){
-            cout << "Press any key to continue..." << endl;
-            break;
-            
+        cout << "Enter a phone number: (Q to quit) ";
+        cin >> dig1;
+        if (dig1 == 'Q'){
+            cin.ignore();
+            cin.get();
+            return 0;
         }
-        int result = readDials(phoneNumber);
-        switch (result){
-            case 0:
-                break;
+        cin >> dig2 >> dig3 >> hyph >> dig4 >> dig5 >> dig6 >> dig7;
+        verify = readDials();
+        switch (verify){
             case -1:
-                cout << "ERROR - An invalid character was entered" << endl;
+                cout << "ERROR - An invalid character was entered" << endl << endl;
                 break;
             case -2:
                 cout << "ERROR - Phone number cannot begin with 0" << endl;
@@ -125,13 +103,13 @@ int main(int argc, const char * argv[]) {
             case -4:
                 cout << "ERROR - Hyphen is not in the correct position" << endl;
                 break;
+            case 0:
+                cout << "Phone Number Dialed: " << dig1 << dig2 << dig3 << hyph << dig4 << dig5 << dig6 << dig7 << endl;
+                break;
         }
+        cout << endl;
         
     }
-    
-    cin.ignore();
-    cin.get();
-    return 0;
-    
-    
+
+   
 }
